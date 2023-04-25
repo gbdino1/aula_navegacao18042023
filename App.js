c40d580d4ea7819b3ed29a26469891272ef55c8d
gbdino1 <gabrielnicolas.dino@gmail.com>
2023-04-25 19:28:48 -0300

    Commit da base do projeto

--- App.js
+++ App.js
@@ -1,12 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
+
+//janela 1
+const Janela1 = ({navigation})=> {
+  return (
+    <View style={styles.container}>
+      <Text>PRIMEIRA JANELA</Text>
+      <TouchableOpacity 
+      style={{
+        borderRadius: 60,
+        borderWidth: 1,
+        borderColor: 'blue',
+        padding: 16,
+        margin:20}}
+      onPress={()=>navigation.navigate('Janela2')}>
+        <Text>TROCA DE JANELA</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+// janela 2
+const Janela2 = ({navigation})=> {
+  return (
+    <View style= {styles.container}>
+      <Text>SEGUNDA JANELA</Text>
+      <TouchableOpacity 
+      style={{
+        borderRadius: 60,
+        borderWidth: 1,
+        borderColor: 'blue',
+        padding: 16,
+        margin:20}}
+      onPress={()=>navigation.navigate('Janela3')}>
+        <Text>TROCA DE JANELA</Text>
+      </TouchableOpacity>
+
+
+    </View>
+  )
+}
+
+// Terceira janela
+const Janela3 = ()=> {
+  return (
+    <View>
+      <Text>Janela 3</Text>
+    </View>
+  )
+}
+
+//>>>>>>>>>>>>
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName='Janela1'>
+      <Stack.Screen name ="Janela1" component={Janela1}
+      options={{title:"Primeira janela",
+       headerTintColor: "#00000055",
+       statusBarColor:"blue",
+       navigationBarColor: 'green'}} />
+      <Stack.Screen name ="Janela2" component={Janela2}/>
+      <Stack.Screen name ="Janela3" component={Janela3}/>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
